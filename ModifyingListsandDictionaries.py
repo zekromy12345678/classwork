20ba9a5d4c96cf9abcc96cac456edb663e0ac309
zekromy12345678 <113252443+zekromy12345678@users.noreply.github.com>
2022-09-15 12:37:14 +0000

    added modify list and dic

---
+++ ModifyingListsandDictionaries.py
@@ -0,0 +1,12 @@
+List = ["coffee", "tea", "milk", "sugar"]
+List[0] = 'redbull'
+print(List)
+
+techdict = {
+"make": "Honda",
+"model": "Civic",
+"year": "2022"
+}
+techdict.update({"make": "ferrari"})
+techdict.update({"model": "Purosangu"})
+print(techdict)
--- Multipleconditions.py
+++ Multipleconditions.py
@@ -11,15 +11,15 @@ elif name[0].lower() == 'c' :
 else:
     last = input('What is your last name? ')
 # If their first name starts with another letter, ask for their last name
-if last[0].lower() == 'z':
-    print('Go to room Z!')
+    if last[0].lower() == 'z':
+        print('Go to room Z!')
 # IF their last name starts with Z, tell them to go to room Z
-else:
-    print('go to room OTHER bozo!')
+    else:
+        print('go to room OTHER bozo!')
 # if their last name starts with any other letter, tell them to go to room OTHER
 # When you are done
 # Anna should be in room AB
 # Bob should be in room AB
 # Charlie should be in room C
 # Khalid Haque should be in room OTHER
-# Xin Zhao should be in room Z
+# Xin Zhao should be in room Zpyth
