068d745c0176eec0dea574a87623145aa321732d
zekromy12345678 <113252443+zekromy12345678@users.noreply.github.com>
2022-09-22 14:52:32 +0000

    included barista bs

---
+++ classandobject.py
@@ -0,0 +1,5 @@
+#Question 1: Create a class for a vehicle with top speed and mileage instance attributes.
+
+#Question 2: Create a class for a computer build with specs attributes
+
+#Question 3: Create a class without any attributes that will move you to the rest of your script
