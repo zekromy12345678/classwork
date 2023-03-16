1af3c161338ef54822f8e7c1704f9a081735bb80
zekromy12345678 <daniely39@nycstudents.net>
2023-03-15 23:07:54 -0400

    clowns

--- testing.py
+++ testing.py
@@ -1,6 +1,6 @@
 from pymarketwatch import MarketWatch
 
-api = MarketWatch("daniel.yadgarov23@sitechhs.com", "Tbd100179", "si-tech2023", True)
+api = MarketWatch("daniel.yadgarov23@sitechhs.com", "Tbd100179", "vc", True)
 api.buy("AAPL", 10)
 
 print(api.get_pending_orders())
