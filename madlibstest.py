4a873f3c4015d2f1a10648887f62a156c7413ab8
zekromy12345678 <daniely39@nycstudents.net>
2023-04-27 19:16:11 -0400

    jhv

---
+++ madlibstest.py
@@ -0,0 +1,44 @@
+import random
+paragraphs = []
+variable_counts = {}
+
+#imports all paragraphs into a list
+def importparagraphs():
+    with open ('madlibs.txt', 'rt') as file:  # Open lorem.txt for reading
+        for x in file:              # For each line, read to a string,
+            paragraphs.append(x)    # and print the string.
+    return paragraphs
+    
+#counts the amount of variables in a selected string
+def count_variables(chosenpara):
+    for word in chosenpara.split():
+        if '<' in word and '>' in word:
+            index1 = word.index("<") +1
+            index2 = word.index(">")
+            variable = word[index1:index2]
+            if variable in variable_counts:
+                variable_counts[variable] += 1
+            else:
+                variable_counts[variable] = 1
+    return variable_counts
+
+#function that gets input and replaces the place holders
+def askvariablesnreplace(chosenpara):
+    c = 0
+    for x in variable_counts.keys():
+        j = variable_counts[x]
+        while c < j:
+            response = input(f"Enter a {x}: ")
+            chosenpara = chosenpara.replace(f"<{x}>", response , 1)
+            c = c+1
+            print(chosenpara)
+        c = 0
+    return chosenpara
+
+importparagraphs()
+chosenpara = paragraphs[random.randint(0,5)]
+count_variables(chosenpara)
+print(chosenpara)
+print(variable_counts)
+askvariablesnreplace(chosenpara)
+print(chosenpara)
